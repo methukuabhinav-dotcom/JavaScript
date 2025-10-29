24272d343c6c0718e805d2bedd72c72bcf5bfae2
Abhinav <methukuabhinav@gmail.com>
2025-10-29 10:30:34 +0530

    JS updated code day-1

---
+++ day-4/functions.js
@@ -0,0 +1,26 @@
+// functons----------------
+// reusability
+// esay to maintain
+
+// types of fumctions---
+// 1.named functions
+// ii.anonomous functions
+// 3.arrow functions
+// 4.iffe(immediatly invoked function expression)
+// 5.genarator function
+// 6.function expressions
+// 7.callback functions
+
+
+// 1.named functions:----------------
+
+// same identifier rules will be applicable
+
+// fn diclarations
+function Data(a,b){//variables that are passed in fn declaration are called as parameters,arrguments.
+    console.log("welcome")
+    return   //returns the value for further use and return statment is the last statment of execuation.
+    // 1.fn with empty return &without return statment will return undifined.
+}
+// fn call
+Data()//these are actula values(all premitive and non primitive) those are called actula arguments.
---
+++ day-4/switch.js
@@ -0,0 +1,97 @@
+// switch case---------
+
+// syntax---
+
+// switch(condition){
+//     case condition:
+//         break;
+//     case condition:
+//         break;
+//     default:
+//         break;
+// }
+
+// let membership="gold"
+// switch(membership){
+//     case "gold":
+//         console.log("10%")
+//         break;
+//     case "silver":
+//         console.log("5%")
+//         break;
+//     default:
+//         console.log("0%")
+//         break;
+// }
+
+// let role="Admin"
+// let action="delete"
+// let newrole=role.toLowerCase
+// switch (role){
+//     case "student":
+//         if (action=="create" || action=="update" || action=="delete"){
+//             console.log("cant performs the actions")
+//         }else{
+//             console.log()
+//         }
+// }
+
+
+// WAP to stimulate the calculater using switch case--------------------
+// let a=100
+// let b=5
+// let cal="+"
+// switch (cal){
+//     case "+":
+//         console.log(a+b)
+//     break;
+//     case "-":
+//         console.log(a-b)
+//     break;
+//     case "/":
+//         console.log(a/b)
+//     break;
+//     case "*":
+//         console.log(a*b)
+//     break;
+//     default:
+//         console.log("not a num")
+//     break;
+// }
+
+// WAP to stimulate the food menu selection in resturent app---------------------------
+// let course="veg"
+// let veg="paneer"
+// let nonveg="chicken"
+// switch (course){
+//     case "veg":
+//         switch (veg){
+//             case "paneer":
+//                 console.log("panner butter masala, kadai paneer")
+//             break;
+//             case "rotis":
+//                 console.log("bread, rummali")
+//             break;
+//             default:
+//                 console.log("other curries")
+//             break;
+//         }
+//     break;
+//     case "nonveg":
+//         switch (nonveg){
+//             case "chicken":
+//                 console.log("chicken masala, kadai chicken")
+//             break;
+//             case "rotis":
+//                 console.log("bread, rummali")
+//             break;
+//             default:
+//                 console.log("other curries")
+//             break;
+//         }
+//     break;
+//     default:
+//         console.log("deserts")
+//     break;
+// }
+
